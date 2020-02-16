526b8d3708e197ce526e38bec12799dcab03235d
Nia-Imani Thomas <61117975+Nia-ImaniThomas@users.noreply.github.com>
2020-02-16 12:59:18 -0600

    Add files via upload

---
+++ Lesson 1-4_My first GitHub_Nia-Imani Thomas.cs
@@ -0,0 +1,9 @@
+﻿A new Git repository has been created for you in C:\Users\student\source\repos\ConsoleApp1.
+Opening repositories:
+C:\Users\student\source\repos\ConsoleApp1
+A new Git repository has been created for you in C:\Users\student\source\repos\Lab 2-4.
+Opening repositories:
+C:\Users\student\source\repos\Lab 2-4
+A new Git repository has been created for you in C:\Users\student\source\repos\My 1st GitHub.
+Opening repositories:
+C:\Users\student\source\repos\My 1st GitHub
